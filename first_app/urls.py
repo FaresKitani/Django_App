163e6955d55564e39dc2feb6e289ccf65371627a
Fares kitani <fariskitany_fir@yahoo.com>
2022-06-04 09:21:18 +0300

    new  commit

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('add',views.add, name='add'),
-    #path('add1',views.home, name='add1'),   using redirect -- need 
+    path('add1',views.home, name='add1'),  # using redirect -- need 
+
+    path('index',views.index, name='index')
 
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import models 
 
 ############
 from django.shortcuts import redirect
@@ -26,11 +26,51 @@ def add(request):
     return render (request,'result.html',{'result':res})
 
 
+def index(request):
+    
+    dest1 = models.Destination()
+    dest1.name = 'Mubai'
+    dest1.desc='The City That Never Sleep!'
+    dest1.img='destination_1.jpg'
+    dest1.price=3400
+
+    dest2 = models.Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc='First Biryani, Then Sherwani'
+    dest2.img='destination_2.jpg'
+    dest2.price=2650
+
+    dest3 = models.Destination()
+    dest3.name = 'Bengaluru'
+    dest3.desc='Beautiful City'
+    dest3.img='destination_3.jpg'
+    dest3.price=1750
+
+    dest4 = models.Destination()
+    dest4.name = 'pares'
+    dest4.desc='The City That Never Sleep!'
+    dest4.img='destination_4.jpg'
+    dest4.price=7600
+
+    dest5 = models.Destination()
+    dest5.name = 'V_City'
+    dest5.desc='First Biryani, Then Sherwani'
+    dest5.img='destination_5.jpg'
+    dest5.price=6500
+
+    dest6 = models.Destination()
+    dest6.name = 'Nables'
+    dest6.desc='Beautiful City'
+    dest6.img='destination_6.jpg'
+    dest6.price=1850
+
+    dests=[dest1,dest2,dest3,dest4,dest5,dest6]
+
+    return render (request,'index.html', {'dests':dests})
 
 
 
-
-###############################3
+###############################
 def add1(request):
     #return render(request,'home.html')
     obj = MyModel.objects.get()
